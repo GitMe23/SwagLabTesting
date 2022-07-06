ea67aeed57dfda30f53233a32657139955bf85a5
Ahmed Yusuf <ahmedyusuf6195@gmail.com>
2022-07-06 16:15:23 +0100

    commiting the first batch of work from my branch

--- SWAGLABTEST/src/test/java/com/sparta/PageObjects/CartPage.java
+++ SWAGLABTEST/src/test/java/com/sparta/PageObjects/CartPage.java
@@ -1,4 +1,28 @@
 package com.sparta.PageObjects;
 
-public class CartPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CartPage extends Page {
+
+    public CartPage (WebDriver webDriver) {
+        super(webDriver);
+        this.webDriver = webDriver;
+    }
+
+    WebDriver webDriver;
+
+    public CheckoutStepOnePage getCheckout() {
+        webDriver.findElement(By.id("checkout")).click();
+;
+        return new CheckoutStepOnePage(webDriver);
+    }
+
+    public boolean checkItemRemoved() {
+        if (webDriver.findElement(By.className("removed_cart_item")) != null) {
+            return true;
+        }
+        return false;
+    }
 }
