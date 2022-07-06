ee3f095e6e5a36f8df998537e1e8bf310371c3c8
WillTran02 <williamqtran@hotmail.co.uk>
2022-07-06 16:39:45 +0100

    Added SetDimensions and ChangeDimensionsTo<device> methods to Page

--- SWAGLABTEST/src/test/java/com/sparta/PageObjects/Page.java
+++ SWAGLABTEST/src/test/java/com/sparta/PageObjects/Page.java
@@ -1,6 +1,7 @@
 package com.sparta.PageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
 public abstract class Page {
@@ -18,6 +19,31 @@ public abstract class Page {
     public String getUrl() {
         return webDriver.getCurrentUrl();
     }
+    public void setDimensions(int width, int height) {
+        webDriver.manage().window().setSize(new Dimension(width, height));
+    }
+    public void changeDimensionsToIPhoneSE() {
+        setDimensions(375, 667);
+    }
+    public void changeDimensionToIPhoneXR() {
+        setDimensions(414, 896);
+    }
+    public void changeDimensionToSamsungGalaxyS8Plus() {
+        setDimensions(360, 740);
+    }
+    public void changeDimensionToIPadAir() {
+        setDimensions(820, 1180);
+    }
+    public void changeDimensionToSurfaceProSeven() {
+        setDimensions(912, 1368);
+    }
+    public void changeDimensionToGalaxyFold() {
+        setDimensions(280, 653);
+    }
+    public void changeDimensionToNestHub() {
+        setDimensions(1024, 600);
+    }
+
     public ExternalPage clickTwitterLink() {
         webDriver.findElement(twitterLink).click();
         return new ExternalPage(webDriver);
