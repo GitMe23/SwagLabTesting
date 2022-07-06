9555e6542f061b6635407414a23885ec82e6924b
WillTran02 <williamqtran@hotmail.co.uk>
2022-07-06 17:02:58 +0100

    Started FrameworkTest, made private Page fields final

--- SWAGLABTEST/src/test/java/com/sparta/FrameWorkTesting/FrameWorkTest.java
+++ SWAGLABTEST/src/test/java/com/sparta/FrameWorkTesting/FrameWorkTest.java
@@ -1,4 +1,34 @@
 package com.sparta.FrameWorkTesting;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+
+import java.io.File;
+import java.io.IOException;
+
 public class FrameWorkTest {
+    private WebDriver driver;
+    private static ChromeDriverService service;
+    @BeforeAll
+    static void setupAll() {
+        service = new ChromeDriverService
+                .Builder()
+                .usingDriverExecutable(new File("src/test/resources/chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver(service);
+        driver.manage().window().maximize();
+    }
 }
